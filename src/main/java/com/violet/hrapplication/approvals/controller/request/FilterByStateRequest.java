809dcbb60a869f26e57ba91fc7cd4dc29ebbca8d
MenekseYuncu <baioretto66@gmail.com>
2024-01-11 22:28:24 +0300

    pagination fixed

--- src/main/java/com/violet/hrapplication/approvals/controller/request/FilterByStateRequest.java
+++
@@ -1,7 +0,0 @@
-package com.violet.hrapplication.approvals.controller.request;
-
-import com.violet.hrapplication.approvals.model.enums.State;
-public record FilterByStateRequest (
-    State state
-){
-}
---
+++ src/main/java/com/violet/hrapplication/approvals/controller/request/PaginationAndFilter.java
@@ -0,0 +1,36 @@
+package com.violet.hrapplication.approvals.controller.request;
+
+import com.violet.hrapplication.approvals.model.enums.State;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+public class PaginationAndFilter {
+
+    @Valid
+    @NotNull
+    private PaginationRequest paginationRequest;
+
+    private final FilterState filterState;
+
+    public static class FilterState {
+        State state;
+
+        public State getState() {
+            return state;
+        }
+    }
+
+    public PaginationAndFilter(PaginationRequest paginationRequest, FilterState filterState) {
+        this.paginationRequest = paginationRequest;
+        this.filterState = filterState;
+    }
+
+
+    public PaginationRequest getPaginationRequest() {
+        return paginationRequest;
+    }
+
+    public FilterState getFilterState() {
+        return filterState;
+    }
+}
